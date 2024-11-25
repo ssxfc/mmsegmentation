8dae5f9283be3d3009cc37908cfa7b8244c89032
zww1919 <2010242074@qq.com>
2024-11-25 21:28:46 +0800

    feat: customize my dataset

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -40,6 +40,8 @@ from .transforms import (CLAHE, AdjustGamma, Albu, BioMedical3DPad,
                          SegRescale)
 from .voc import PascalVOCDataset
 
+from .ebhi import EBHIDataset
+
 # yapf: enable
 __all__ = [
     'BaseSegDataset', 'BioMedical3DRandomCrop', 'BioMedical3DRandomFlip',
@@ -60,5 +62,5 @@ __all__ = [
     'MapillaryDataset_v2', 'Albu', 'LEVIRCDDataset',
     'LoadMultipleRSImageFromFile', 'LoadSingleRSImageFromFile',
     'ConcatCDInput', 'BaseCDDataset', 'DSDLSegDataset', 'BDD100KDataset',
-    'NYUDataset'
+    'NYUDataset', 'EBHIDataset'
 ]
